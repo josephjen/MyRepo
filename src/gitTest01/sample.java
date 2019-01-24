3a8fb19ad6fa251c0d92a0dcbc1dc6bee4ff6c0e
josephjen <josephselvaraj0802@gmail.com>
2019-01-24 13:23:15 +0530

    Life time menus printed

--- src/gitTest01/sample.java
+++ src/gitTest01/sample.java
@@ -1,10 +1,30 @@
 package gitTest01;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my first git push");
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\joe\\Selenium\\driver\\chromedriver.exe");
+		WebDriver wd =  new ChromeDriver();
+		wd.get("https://www.lifetime.life/");
+		wd.manage().window().maximize();
+		wd.manage().timeouts().implicitlyWait(150, TimeUnit.SECONDS);
+		wd.findElement(By.xpath("//ul[@id='primaryNav']/li[3]/a")).click();
+		List <WebElement> links =  wd.findElements(By.xpath("//ul[@id='primaryNav']/li[3]//div[2]/ul/li"));
+		//WebElement w = wd.findElement(By.xpath("//ul[@id='primaryNav']/li[3]//div[2]//li"));
+		for (int i = 1; i < links.size(); i++) {
+			System.out.println("Element.-"+i + "= " +links.get(i).getText());
+			
+	}
 	}
 
 }
